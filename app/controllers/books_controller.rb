8682fc3c91d8a0fc5df4c01f347dabe8da4afe74
EC2 Default User <ec2-user@ip-172-31-29-153.ap-northeast-1.compute.internal>
2023-04-29 07:25:38 +0000

    cssが一番だるい

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,9 +33,9 @@ class BooksController < ApplicationController
 
 
   def show
+    @book_detail = Book.find(params[:id])
     @user = User.find(@book_detail.user_id)
     @book = Book.new
-    @book_detail = Book.find(params[:id])
   end
 
   def edit
